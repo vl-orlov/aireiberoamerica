424a389c2190a1f55bd5575c13dc456f54cbc75c
Lev Orlov <lev.orlov@dif.tech>
2026-03-09 21:55:30 -0300

    Fix base code

--- web/includes/home.php
+++ web/includes/home.php
@@ -1,3 +1,16 @@
 <div class="logo_cont">
-  <img src="img/logo.png" class="logo_img">
+  <div class="under_construction_block">
+    <img src="img/logo.png" class="logo_img" alt="AIRE">
+    <div class="construction_message">
+      <span class="construction_icon" aria-hidden="true">
+        <!-- hard hat / construction -->
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+          <path d="M12 2c-2 0-4 1.5-4 4v2H4v9h16v-9h-4V6c0-2.5-2-4-4-4z"/>
+          <path d="M8 8h8"/>
+        </svg>
+      </span>
+      <span class="construction_text">Sitio en construcción</span>
+      <span class="construction_sub">Próximamente</span>
+    </div>
+  </div>
 </div>
